f7dd77d93a3480f886a44b76da7391042f38a472
jawad khan <73393333+jkbells@users.noreply.github.com>
2023-03-10 02:21:32 +0500

    node

---
+++ intro-to-node/index.js
@@ -0,0 +1,10 @@
+var superheros = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheros.random();
+var mySupervillains = supervillains.random();
+
+console.log(mySuperheroName); 
+console.log(mySupervillains);
+
+
